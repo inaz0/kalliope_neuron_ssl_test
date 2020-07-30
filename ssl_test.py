5888c665c9b156ba2aedebc3f579e888db833cd7
inaz0 <inazo.wh@gmail.com>
2020-07-30 16:12:37 +0200

    Example et upgrade
    Ajout du répertoire exemple pour le brain et le template, et début des modifications pour attendre la fin du test  et annoncer le résultat

--- ssl_test.py
+++ ssl_test.py
@@ -16,16 +16,21 @@ class Ssl_test(NeuronModule):
     def __init__(self, **kwargs):
         super(Ssl_test, self).__init__(**kwargs)
         # Basic configuration
-        self.host = 'https://www.ssllabs.com/ssltest/analyze.html?d=test.com&hideResults=on&latest='
-        self.port = kwargs.get('port', 80)
-        self.hideResults = kwargs.get('hideResults', 'on')
-        self.password = kwargs.get('password', None)
         self.query = kwargs.get('query', None)
         r = requests.get('https://www.ssllabs.com/ssltest/analyze.html?d=' + self.query + '&hideResults=on&latest=')
+		
         self.returnCode = r.status_code
+		
         self.message = {
         "summary": "Le test est lancé !",
         "returncode": self.returnCode
         }
 
         self.say(self.message)
+		
+		m = re.search('Please wait..', r.text)
+		
+		self.message = {
+        "summary": m.group(0),
+        "returncode": self.returnCode
+        }
