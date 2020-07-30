cd8f6d45483f7906f25ebfa1366d103510fd42c7
inaz0 <inazo.wh@gmail.com>
2020-07-30 12:19:51 +0200

    Initial push
    init push

---
+++ dna.py
@@ -0,0 +1,6 @@
+name: "ssl_test"
+type: "neuron"
+author: "inaz0"
+
+kalliope_supported_version:
+    - 0.7
---
+++ ssl_test.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import re
+import sys  
+import datetime
+import requests
+import urllib.request as urllib_request
+
+import urllib
+
+from time import sleep
+from kalliope.core.NeuronModule import NeuronModule, MissingParameterException
+from kalliope import Utils
+
+
+class SSLTest(NeuronModule):
+    def __init__(self, **kwargs):
+        super(SSLTest, self).__init__(**kwargs)
+		
+        # Basic configuration
+        self.host = 'https://www.ssllabs.com/ssltest/analyze.html?d=test.com&hideResults=on&latest=')
+        self.port = kwargs.get('port', 80)
+        self.hideResults = kwargs.get('hideResults', 'on')
+        self.password = kwargs.get('password', None)
+
+		self.query = kwargs.get('domainSeek', None)
+		
+		r = requests.get('https://www.ssllabs.com/ssltest/analyze.html?d=' + self.query + '&hideResults=on&latest=')
+		self.returnCode = r.status_code
+   
+		self.message = {
+			"summary": "Le test est lancé !",
+			"returncode": self.returnCode
+		}
+		
+		self.say(self.message)
